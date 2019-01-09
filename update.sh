bb3015c028f48cdc73d6c900de02337403521d03
cuu <gnubsd@me.com>
2019-01-09 18:37:11 +0800

    update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 feh --bg-center /home/cpi/launchergo/sysgo/gameshell/wallpaper/updating.png
 cd /home/cpi/launchergo 
-git pull
+git pull --depth=1
 git reset --hard $1
 feh --bg-center /home/cpi/launchergo/sysgo/gameshell/wallpaper/loading.png 
 ./load.sh
